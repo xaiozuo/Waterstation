d8347e1e9294fa35c25d3c742482fce8ea946023
laizl <2396637487@qq.com>
2024-06-16 18:51:29 +0800

    发起付款支付

--- src/main/java/com/waterstation/waterstation/controller/PayController.java
+++ src/main/java/com/waterstation/waterstation/controller/PayController.java
@@ -10,7 +10,6 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
